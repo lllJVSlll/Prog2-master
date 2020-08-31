aa76974b5de62493bb566a3a13286ad45d7c10b3
lllJVSlll <johan.simonsen@elev.ga.ntig.se>
2020-08-31 12:17:25 +0200

    initial commit
    hewm

---
+++ Prog 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prog_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Niklas!");
+        }
+    }
+}
