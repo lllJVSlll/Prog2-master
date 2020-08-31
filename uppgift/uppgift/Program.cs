69b97b2099ca1fefffd6e64fa5a1d26c0eb9c8e1
lllJVSlll <johan.simonsen@elev.ga.ntig.se>
2020-08-31 12:45:56 +0200

    läxa 2

---
+++ uppgift/uppgift/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in användarman: ");
+
+            string username = Console.ReadLine();
+
+            Console.WriteLine("Användarman: "+ username);
+
+            Console.WriteLine("Skriv in Ålder: ");
+
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Din ålder är" + age);
+
+            Console.WriteLine("Är du vid liv?");
+                string alive = Console.ReadLine();
+                var ja = true;
+            var nej = false;
+
+            Console.WriteLine("Är du vid liv?" + alive);
+
+        }
+    }
+}
