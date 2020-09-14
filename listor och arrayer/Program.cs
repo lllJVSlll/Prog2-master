04b12a15b4d421266fd4f3185abdad2adcb8bb40
lllJVSlll <johan.simonsen@elev.ga.ntig.se>
2020-09-14 12:26:16 +0200

    gjort klart

---
+++ listor och arrayer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace listor_och_arrayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nameinput = new string[5];
+
+
+            Console.WriteLine("Skriv in 5 namn: ");
+
+            for (int i = 0; i < nameinput.Length; i++)
+            {
+                nameinput[i] = Console.ReadLine();
+            }
+            Array.Reverse(nameinput);
+                
+            Console.WriteLine("Dina namn är ");
+
+            for (int i = 0; i < nameinput.Length; i++)
+            {
+                Console.WriteLine(nameinput[i]);
+                
+            }
+            
+        }
+    }
+}
